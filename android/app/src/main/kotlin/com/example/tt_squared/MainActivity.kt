03e63f314e534679e9a5149e00ee83020435d197
Hershey49 <76010332+Hershey49@users.noreply.github.com>
2023-11-19 09:36:22 -0500

    Created base gamepage screen with placeholders

---
+++ android/app/src/main/kotlin/com/example/tt_squared/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tt_squared
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tic Tac Toe',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Tic Tac Toe Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(9, (index) {
+          return Center(
+            child: Text(
+              'Board ${index + 1}',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
